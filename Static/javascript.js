bae08363507e91f801d5de2574789120c7cbcf5f
svsannidhay <sannidhayvashalsv@gmail.com>
2020-11-23 17:30:28 +0530

    Image generator section added

--- Static/javascript.js
+++ Static/javascript.js
@@ -11,3 +11,34 @@ function generateAgeInDays() {
 function ageInDaysReset() {
   document.getElementById("ageDays").remove();
 }
+
+function createColumnWithImage(){
+  let col = document.createElement('div');
+  col.setAttribute("class","col span-1-of-4 imageContainer");
+  let img = document.createElement('img');
+  img.setAttribute("src","/Static/images/header_background.jpg");
+  img.setAttribute("alt","Js code image");
+  col.appendChild(img);
+  return col;
+}
+
+var countgenerate = 0;
+var lastrow = -1;
+function generateImage(){
+  countgenerate++;
+  let lastRowNo = Math.floor(countgenerate/5);
+  if( (lastrow!= -1)  && ((countgenerate-1) % 4 !=0) ){
+    document.getElementById("rowno"+ lastRowNo).remove();
+  }
+  let row = document.createElement("div");
+  row.setAttribute("class","row imageRow");
+  row.setAttribute("id", "rowno" + lastRowNo);
+  let countcol = countgenerate % 4;
+  if(countcol==0) countcol = 4;
+  for(let i=1;i<=countcol;i++){
+    let col = createColumnWithImage();
+    row.appendChild(col);
+  }
+  lastrow = lastRowNo;
+  document.getElementById("imageGenerator").appendChild(row);
+}
